6bacdbde4486010823ebffa34ec79aab49d4e144
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-18 11:00:29 -0300

    i add ensureAuth middleware to user.route.ts

--- src/infrastructure/router/user/user.route.ts
+++ src/infrastructure/router/user/user.route.ts
@@ -2,6 +2,7 @@ import { Express } from "express";
 import { SequelizeRepository } from "../../repository/user/sequelize-user.repository";
 import { UserUseCase } from "../../../application/user/user-use-case";
 import { UserController } from "../../../infrastructure/controller/user/user.controller";
+import { ensureAuth } from "../../middleware/auth.middleware";
 import SocketAdapter from "../../services/socketAdapter";
 
 function configureUserRoutes(app: Express, socketAdapter: SocketAdapter) {
@@ -21,13 +22,13 @@ function configureUserRoutes(app: Express, socketAdapter: SocketAdapter) {
     const userCtrl = new UserController(userUseCase, socketAdapter);
     
     /*
-    *   Registrar rutas
+    *   Registrar rutas protegidas con ensureAuth
     */
-    app.get(`/${process.env.BASE_URL_API}/users/:filter?/:page?/:perPage?`, userCtrl.getAllCtrl);
-    app.get(`/${process.env.BASE_URL_API}/user/:usr_uuid`, userCtrl.getCtrl);
-    app.post(`/${process.env.BASE_URL_API}/user`, userCtrl.saveCtrl);
-    app.put(`/${process.env.BASE_URL_API}/user/:usr_uuid`, userCtrl.updateCtrl);
-    app.delete(`/${process.env.BASE_URL_API}/user/:usr_uuid`, userCtrl.deleteCtrl);
+    app.get(`/${process.env.BASE_URL_API}/users/:filter?/:page?/:perPage?`, ensureAuth, userCtrl.getAllCtrl);
+    app.get(`/${process.env.BASE_URL_API}/user/:usr_uuid`, ensureAuth, userCtrl.getCtrl);
+    app.post(`/${process.env.BASE_URL_API}/user`, ensureAuth, userCtrl.saveCtrl);
+    app.put(`/${process.env.BASE_URL_API}/user/:usr_uuid`, ensureAuth, userCtrl.updateCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/user/:usr_uuid`, ensureAuth, userCtrl.deleteCtrl);
 }
 
 export default configureUserRoutes;
