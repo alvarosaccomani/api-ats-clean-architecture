4b15588b7696ef88f9fa2981f62f3c21e742c24c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-17 23:32:37 -0300

    i add user.repository.ts into user folder into folder domain to project

---
+++ src/domain/user/user.repository.ts
@@ -0,0 +1,14 @@
+import { UserEntity, UserUpdateData } from "./user.entity";
+
+export interface UserRepository {
+    getUsers(): Promise<UserEntity[] | null>;
+    findUserById(usr_uuid: string): Promise<UserEntity | null>;
+    findUserByEmail(usr_email: string): Promise<UserEntity | null>;
+    saveUser(user: UserEntity): Promise<UserEntity | null>;
+    updateUser(usr_uuid: string, user: UserUpdateData): Promise<UserEntity | null>;
+    deleteUser(usr_uuid: string): Promise<UserEntity | null>;
+    userExist(usr_nick: string, usr_email: string): Promise<UserEntity | null>;
+    setSocketUser(usr_uuid: string, usr_socket: string, usr_online: boolean): Promise<UserEntity | null>;
+    findUserByNick(usr_nick: string): Promise<UserEntity | null>;
+    findSocketUser(usr_uuid: string): Promise<string | null>;
+}
