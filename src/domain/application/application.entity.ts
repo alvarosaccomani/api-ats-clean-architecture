81565490677d47d78874c36b3cc114d2e1598f33
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-20 11:33:06 -0300

    i add application.entity.ts into application folder into folder domain to project

---
+++ src/domain/application/application.entity.ts
@@ -0,0 +1,16 @@
+export interface ApplicationEntity {
+    app_uuid: string,
+    app_cod: string,
+    app_name: string,
+    tapp_uuid: string,
+    app_description: string,
+    app_dbname: string,
+    app_url: string,
+    app_hasaccess: boolean,
+    app_active: boolean,
+    app_createdat: Date,
+    app_updatedat: Date
+}
+
+//Update
+export type ApplicationUpdateData = Pick<ApplicationEntity, 'app_cod' | 'app_name' | 'tapp_uuid' | 'app_description' | 'app_dbname' | 'app_url' | 'app_hasaccess' | 'app_active'>
