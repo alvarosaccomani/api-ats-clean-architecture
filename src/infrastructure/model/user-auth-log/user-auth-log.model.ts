3a2da66d075b8212541c231d4b28abc8ea60ac45
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-21 13:27:33 -0300

    i add user-auth-log.model.ts into user-auth-log folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/user-auth-log/user-auth-log.model.ts
@@ -0,0 +1,64 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { UserAuthLogEntity } from '../../../domain/user-auth-log/user-auth-log.entity';
+import { SequelizeUser } from '../user/user.model';
+import { SequelizeApplication } from '../application/application.model';
+
+export class SequelizeUserAuthLog extends Model<UserAuthLogEntity> {
+  declare usraulo_uuid: string;
+  declare usr_uuid: string;
+  declare app_uuid: string;
+  declare usraulo_action: string;
+  declare usraulo_ipaddress: string;
+  declare usraulo_useragent: string;
+  declare usraulo_failurereason: string;
+  declare usraulo_createdat: Date;
+}
+
+SequelizeUserAuthLog.init({
+  usraulo_uuid: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+  usr_uuid: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  app_uuid: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  usraulo_action: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  usraulo_ipaddress: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  usraulo_useragent: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  usraulo_failurereason: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  usraulo_createdat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  sequelize,
+  timestamps: false,
+  tableName: 'usraulo_UsersAuthLogs'
+});
+
+// Relaciones
+SequelizeUserAuthLog.belongsTo(SequelizeUser, { foreignKey: 'usr_uuid', as: 'user' });
+SequelizeUserAuthLog.belongsTo(SequelizeApplication, { foreignKey: 'app_uuid', as: 'application' });
+
+// Sincronizar (solo en desarrollo)
+if (process.env.NODE_ENV !== "production") {
+    SequelizeUserAuthLog.sync();
+}
