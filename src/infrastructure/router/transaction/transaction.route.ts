a3a415f6c4e217802252fd51dfb4b09001c4c2fa
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-21 23:11:17 -0300

    i add transaction.route.ts into transaction folder into folder router into folder infrastructure to project

---
+++ src/infrastructure/router/transaction/transaction.route.ts
@@ -0,0 +1,34 @@
+import { Express } from "express";
+import { SequelizeTransactionRepository } from "../../repository/transaction/sequelize-transaction.repository";
+import { TransactionUseCase } from "../../../application/transaction/transaction-use-case";
+import { TransactionController } from "../../controller/transaction/transaction.controller";
+import { ensureAuth } from "../../middleware/auth.middleware";
+
+function configureTransactionRoutes(app: Express) {
+    /*
+    *   Iniciar repository
+    */
+    const sequelizeTransactionRepo = new SequelizeTransactionRepository();
+    
+    /*
+    *   Iniciar casos de uso
+    */
+    const transactionUseCase = new TransactionUseCase(sequelizeTransactionRepo);
+    
+    /*
+    *   Iniciar controller
+    */
+    const transactionCtrl = new TransactionController(transactionUseCase);
+    
+    /*
+    *   Registrar rutas protegidas con ensureAuth
+    */
+    app.get(`/${process.env.BASE_URL_API}/transactions`, ensureAuth, transactionCtrl.getAllCtrl);
+    app.get(`/${process.env.BASE_URL_API}/transactions/subscriber/:type/:id`, ensureAuth, transactionCtrl.getBySubscriberCtrl);
+    app.get(`/${process.env.BASE_URL_API}/transaction/:trn_uuid`, ensureAuth, transactionCtrl.getCtrl);
+    app.post(`/${process.env.BASE_URL_API}/transaction`, ensureAuth, transactionCtrl.saveCtrl);
+    app.put(`/${process.env.BASE_URL_API}/transaction/:trn_uuid`, ensureAuth, transactionCtrl.updateCtrl);
+    app.delete(`/${process.env.BASE_URL_API}/transaction/:trn_uuid`, ensureAuth, transactionCtrl.deleteCtrl);
+}
+
+export default configureTransactionRoutes;
