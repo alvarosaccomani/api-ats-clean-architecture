118433b782cf87740984f08479792be93be470be
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-28 20:53:17 -0300

    i add application-setting.model.ts into application-setting folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/application-setting/application-setting.model.ts
@@ -0,0 +1,79 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { ApplicationSettingEntity } from '../../../domain/application-setting/application-setting.entity';
+
+export class SequelizeApplicationSetting extends Model<ApplicationSettingEntity> {
+  declare app_uuid: string;
+  declare apps_uuid: string;
+  declare apps_key: string;
+  declare apps_parameter: string;
+  declare apps_description: string;
+  declare apps_value: string;
+  declare apps_datatype: string;
+  declare apps_options: string;
+  declare apps_group: string;
+  declare apps_createdat: Date;
+  declare apps_updatedat: Date;
+}
+
+SequelizeApplicationSetting.init({
+  app_uuid: {
+    type: DataTypes.STRING(40),
+    allowNull: false,
+    primaryKey: true
+  },
+  apps_uuid: {
+    type: DataTypes.STRING(40),
+    allowNull: false,
+    primaryKey: true
+  },
+  apps_key: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  },
+  apps_parameter: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  apps_description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  apps_value: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  apps_datatype: {
+    type: DataTypes.STRING(30),
+    allowNull: true,
+    defaultValue: 'string'
+  },
+  apps_options: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  apps_group: {
+    type: DataTypes.STRING(50),
+    allowNull: true,
+    defaultValue: 'General'
+  },
+  apps_createdat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  apps_updatedat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  sequelize,
+  timestamps: true,
+  createdAt: 'apps_createdat',
+  updatedAt: 'apps_updatedat',
+  tableName: 'app_applicationssettings'
+});
+
+// Sincronizar (solo en desarrollo)
+if (process.env.NODE_ENV !== "production") {
+    SequelizeApplicationSetting.sync();
+}
