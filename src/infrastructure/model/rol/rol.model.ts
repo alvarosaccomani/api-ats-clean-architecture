40cfb80e03a0f6848fb75afdd5ca10a95521a089
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-08-05 15:30:39 -0300

    i add rol.model.ts into rol folder into folder model into folder infrastructure to project

---
+++ src/infrastructure/model/rol/rol.model.ts
@@ -0,0 +1,40 @@
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../db/sequelize';
+import { RolEntity } from "../../../domain/rol/rol.entity";
+
+export class SequelizeRole extends Model<RolEntity> {
+  declare rol_uuid: string;
+  declare rol_name: string;
+  declare rol_createdat: Date;
+  declare rol_updatedat: Date;
+}
+
+SequelizeRole.init({
+  rol_uuid: {
+    type: DataTypes.STRING(40), 
+    primaryKey: true
+  }, 
+  rol_name: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true
+  },
+  rol_createdat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  },
+  rol_updatedat: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+}, {
+  sequelize,
+  timestamps: true,
+  createdAt: 'rol_createdat',
+  updatedAt: 'rol_updatedat',
+  tableName: 'rol_roles'
+});
+
+if (process.env.NODE_ENV !== "production") {
+  SequelizeRole.sync();
+}
