a59a5c4ab9533c68607d1e609e321a002baab5ad
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-08-05 18:09:24 -0300

    i add rol.controller.ts into rol folder into folder controller into folder infrastructure to project

---
+++ src/infrastructure/controller/rol/rol.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { SequelizeRole } from "../../model/rol/rol.model";
+
+export class RolController {
+    public async getAllCtrl(req: Request, res: Response) {
+        try {
+            const roles = await SequelizeRole.findAll({
+                order: [['rol_name', 'ASC']]
+            });
+            return res.status(200).json({
+                success: true,
+                message: 'Roles retornados.',
+                data: roles
+            });
+        } catch (error: any) {
+            console.error('Error en getAllCtrl (RolController):', error.message);
+            return res.status(400).json({
+                success: false,
+                message: 'No se pudieron recuperar los roles.',
+                error: error.message
+            });
+        }
+    }
+}
