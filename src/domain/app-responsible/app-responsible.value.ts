bab84289f6d64cf6278872b11d4f4fabc635b4fe
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-08-05 15:35:26 -0300

    i add app-responsible.value.ts into app-responsible folder into folder domain to project

---
+++ src/domain/app-responsible/app-responsible.value.ts
@@ -0,0 +1,28 @@
+import { v4 as uuid } from "uuid";
+import { AppResponsibleEntity } from "./app-responsible.entity";
+
+export class AppResponsibleValue implements AppResponsibleEntity {
+    appres_uuid: string;
+    app_uuid: string;
+    usr_uuid: string;
+    rol_uuid: string;
+    appres_createdat: Date;
+    appres_updatedat: Date;
+
+    constructor({
+        app_uuid,
+        usr_uuid,
+        rol_uuid
+    }: {
+        app_uuid: string;
+        usr_uuid: string;
+        rol_uuid: string;
+    }) {
+        this.appres_uuid = uuid();
+        this.app_uuid = app_uuid;
+        this.usr_uuid = usr_uuid;
+        this.rol_uuid = rol_uuid;
+        this.appres_createdat = new Date();
+        this.appres_updatedat = new Date();
+    }
+}
