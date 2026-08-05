8a438c8b1fdbf9b78daf2867db09606cdc73f2e6
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-08-05 15:35:26 -0300

    i add app-responsible.entity.ts into app-responsible folder into folder domain to project

---
+++ src/domain/app-responsible/app-responsible.entity.ts
@@ -0,0 +1,8 @@
+export interface AppResponsibleEntity {
+    appres_uuid: string;
+    app_uuid: string;
+    usr_uuid: string;
+    rol_uuid: string;
+    appres_createdat?: Date;
+    appres_updatedat?: Date;
+}
