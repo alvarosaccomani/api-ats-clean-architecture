684b8689a3982521a4cf1b7b25ce0150066050b8
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-08-05 13:33:06 -0300

    i add SequelizeTicket and SequelizeUser to associations.ts

--- src/infrastructure/model/associations.ts
+++ src/infrastructure/model/associations.ts
@@ -1,6 +1,8 @@
 import { SequelizeApplication } from './application/application.model';
 import { SequelizeTypeApplication } from './type-application/type-application.model';
 import { SequelizeApplicationSetting } from './application-setting/application-setting.model';
+import { SequelizeTicket } from './ticket/ticket.model';
+import { SequelizeUser } from './user/user.model';
 
 export function setupAssociations() {
   // Relación entre Aplicación y Tipo de Aplicación
@@ -24,5 +26,12 @@ export function setupAssociations() {
     foreignKey: 'app_uuid',
     as: 'application'
   });
+
+  // Relación entre Ticket, Usuario y Aplicación
+  SequelizeTicket.belongsTo(SequelizeUser, { foreignKey: 'usr_uuid', as: 'user' });
+  SequelizeUser.hasMany(SequelizeTicket, { foreignKey: 'usr_uuid', as: 'tickets' });
+
+  SequelizeTicket.belongsTo(SequelizeApplication, { foreignKey: 'app_uuid', as: 'application' });
+  SequelizeApplication.hasMany(SequelizeTicket, { foreignKey: 'app_uuid', as: 'tickets' });
 }
 
